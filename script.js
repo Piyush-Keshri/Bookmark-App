41c949c7e74e74d84a1fa8818f088f12fcafb6e8
Piyush Keshri <piyushkeshri383@gmail.com>
2022-02-11 20:01:07 +0530

    commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
-
+let bookmarks = [];
 // Show Modal,Focus on Input
 
 function showModal(){
@@ -17,4 +17,108 @@ function showModal(){
 // Modal Event Listeners
 modalShow.addEventListener('click',showModal);
 modalClose.addEventListener('click',() => modal.classList.remove('show-modal'));
-window.addEventListener('click',(e) => console.log(e.target === modal? modal.classList.remove('show-modal'): false));
+window.addEventListener('click',(e) => e.target === modal? modal.classList.remove('show-modal'): false);
+
+// Validate Form
+function validate(nameValue,urlValue){
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+
+    if(!nameValue || !urlValue){
+        alert('Please Submit Values For Both Fields.');
+        return false;
+    }
+
+    if(!urlValue.match(regex)){
+        alert('Please Provide a valid Web Address');
+        return false;  
+    }
+    //Valid
+    return true;
+}
+
+// Build Bookmarks DOM
+function buildBookmarks(){
+    // Build Items
+    bookmarks.forEach((bookmark) => {
+
+        const {name,url} = bookmark;
+        // Item
+        const item = document.createElement('div');
+        item.classList.add('item');
+
+        // Close Icon
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fas','fa-times');
+        closeIcon.setAttribute('title','Delete Bookmark');
+        closeIcon.setAttribute('onclick',`deleteBookmark('${url}')`);
+
+        // Link Container
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+
+        // Favicon
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src',`https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt','Favicon');
+
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href',`${url}`);
+        link.setAttribute('target','_blank');
+        link.textContent = name;
+
+        // Append to bookmarks Container
+        linkInfo.append(favicon,link);
+        item.append(closeIcon,linkInfo);
+        bookmarksContainer.appendChild(item);
+    });
+}
+
+// Fetch BookMarks From Local Storage
+function fetchBookmarks(){
+    // Get Bookmarks from local storage if available
+    if(localStorage.getItem('bookmarks')){
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else{
+        // Create bookmarks array in localStorage
+        bookmarks = [
+            {
+                name : 'google',
+                url : 'https://google.com',
+            },
+        ];
+        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+}
+
+// Handle data from Form
+function storeBookmark(e){
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if(!urlValue.includes('http://','https://')){
+        urlValue = `https://${urlValue}`;
+    }
+    
+    if(!validate(nameValue,urlValue)){
+        return false;
+    };
+
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit',storeBookmark);
+
+// On Load Fetch BookMarks
+fetchBookmarks();
